e7f6b4cd802a29e22156402106ca2aea8a8e7418
Samuel Githengi <samuel.githengi@gmail.com>
2018-06-14 16:39:20 +0300

    Docker compose copy sql scripts to the migrations container

--- composed/sh/start.sh
+++ composed/sh/start.sh
@@ -1,32 +1,22 @@
 #!/bin/bash
 
-# Import data
-
 MYSQL_HOST=mysql
-MYSQL_PORT=3306
-
-echo "wait for mysql to be ready"
-
-while ! nc -q 1 ${MYSQL_HOST} ${MYSQL_PORT} </dev/null;
-do
-  echo "Waiting for database"
-  sleep 10;
-done
 
 # create openmrs properties file
 echo "Creating openmrs properties file"
 
-touch /opt/tomcat/.OpenMRS/openmrs-runtime.properties
-cat > /opt/tomcat/.OpenMRS/openmrs-runtime.properties <<- EOF
-	connection.username=${MYSQL_OPENMRS_USER}
-	connection.password=${MYSQL_OPENMRS_PASSWORD}
-	connection.url=jdbc:mysql://${MYSQL_HOST}:3306/${MYSQL_OPENMRS_DATABASE}?autoReconnect=true&sessionVariables=storage_engine=InnoDB&useUnicode=true&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull
-	module.allow_web_admin=true
-	auto_update_database=false
-	sync.mandatory=false
-EOF
+if [ ! -f /opt/tomcat/.OpenMRS/openmrs-runtime.properties ]; then
+	cat > /opt/tomcat/.OpenMRS/openmrs-runtime.properties <<- EOF
+		connection.username=${MYSQL_OPENMRS_USER}
+		connection.password=${MYSQL_OPENMRS_PASSWORD}
+		connection.url=jdbc:mysql://${MYSQL_HOST}:3306/${MYSQL_OPENMRS_DATABASE}?autoReconnect=true&sessionVariables=storage_engine=InnoDB&useUnicode=true&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull
+		module.allow_web_admin=true
+		auto_update_database=false
+		sync.mandatory=false
+	EOF
 
-echo "Finished creating openmrs properties file"
+	echo "Finished creating openmrs properties file"
 
+fi
 
 exec /usr/bin/supervisord
