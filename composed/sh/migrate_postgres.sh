193f882b14f0cf228ca0bceb04cd839a6cf37fd1
Samuel Githengi <samuel.githengi@gmail.com>
2018-06-14 18:22:47 +0300

    Code cleanup

--- composed/sh/migrate_postgres.sh
+++ composed/sh/migrate_postgres.sh
@@ -41,7 +41,7 @@ groupadd -r postgres --gid=999 && useradd -r -g postgres --uid=999 postgres
 
 chown -R postgres:postgres $POSTGRES_OPENSRP_TABLESPACE_DIR
 
-/opt/mybatis-migrations-3.3.4/bin/migrate up --path /migrate
+/opt/mybatis-migrations-3.3.4/bin/migrate up --path=/migrate
 
 if [ -d /tmp/opensrp-server-${OPENSRP_SERVER_TAG}/assets/tbreach_default_view_configs -a ! -f /etc/migrations/.setup_view_configs_complete ]; then
 	/tmp/opensrp-server-${OPENSRP_SERVER_TAG}/assets/tbreach_default_view_configs/setup_view_configs.sh -t postgres  -u $POSTGRES_OPENSRP_USER -pwd $POSTGRES_OPENSRP_PASSWORD -d $POSTGRES_OPENSRP_DATABASE -h postgres -f /tmp/opensrp-server-${OPENSRP_SERVER_TAG}/assets/tbreach_default_view_configs
