7528855ff4fc31180d9139d2ea4b14c3d05e773c
Samuel Githengi <samuel.githengi@gmail.com>
2018-06-29 15:37:50 +0300

    Copy war files to volume in entry point script

--- composed/sh/entrypoint-migrations.sh
+++ composed/sh/entrypoint-migrations.sh
@@ -1,4 +1,35 @@
 #!/bin/bash
+mv /tmp/opensrp*.war  /tmp/openmrs*.war /etc/migrations 
+
+if [ -f /tmp/opensrp${APPLICATION_SUFFIX}.war ]; then
+	mv /tmp/opensrp${APPLICATION_SUFFIX}.war /etc/migrations 
+	echo "Shared opensrp war to runtime container"
+fi
+
+if [ -f /tmp/openmrs${APPLICATION_SUFFIX}.war ]; then
+	mv /tmp/openmrs${APPLICATION_SUFFIX}.war /etc/migrations 
+	echo "Shared openmrs war to runtime container"
+fi
+
+MYSQL_HOST=mysql
+
+# create openmrs properties file
+echo "Creating openmrs properties file"
+
+if [ ! -f /etc/migrations/openmrs${APPLICATION_SUFFIX}-runtime.properties ]; then
+	cat > /etc/migrations/openmrs${APPLICATION_SUFFIX}-runtime.properties <<- EOF
+		connection.username=${MYSQL_OPENMRS_USER}
+		connection.password=${MYSQL_OPENMRS_PASSWORD}
+		connection.url=jdbc:mysql://${MYSQL_HOST}:3306/${MYSQL_OPENMRS_DATABASE}?autoReconnect=true&sessionVariables=storage_engine=InnoDB&useUnicode=true&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull
+		module.allow_web_admin=true
+		auto_update_database=false
+		sync.mandatory=false
+	EOF
+
+	echo "Finished creating openmrs properties file"
+
+fi
+
 cd $(dirname $0)
 ./migrate_postgres.sh
 ./migrate_mysql.sh
--- composed/sh/start.sh
+++ composed/sh/start.sh
@@ -6,17 +6,17 @@ while [ ! -f /etc/migrations/.postgres_migrations_complete -o ! -f /etc/migratio
 done;
 
 if ls /etc/migrations/opensrp*.war 1> /dev/null 2>&1; then
-	mv /etc/migrations/opensrp*.war /opt/tomcat/instances/openmrs/webapps
+	mv /etc/migrations/opensrp*.war /opt/tomcat/instances/opensrp/webapps
 	echo 'Copying opensrp war'
 fi
 
-if ls /etc/migrations/opensrp*.war 1> /dev/null 2>&1; then
+if ls /etc/migrations/openmrs*.war 1> /dev/null 2>&1; then
 	mv /etc/migrations/openmrs*.war /opt/tomcat/instances/openmrs/webapps
 	echo 'Copying openmrs war'
 fi
 
 if ls /etc/migrations/openmrs*-runtime.properties 1> /dev/null 2>&1; then
-	mv /etc/migrations/openmrs*-runtime.properties /opt/tomcat/.OpenMRS
+	cp /etc/migrations/openmrs*-runtime.properties /opt/tomcat/.OpenMRS
 	echo 'Copying openmrs runtime properties'
 fi
 
