e7033da28242b4f9dc151b0c829adc002643e3d9
Samuel Githengi <samuel.githengi@gmail.com>
2018-06-29 10:42:08 +0300

    Move compiling opensrp and downloading openmrs to migrations container

--- composed/sh/start.sh
+++ composed/sh/start.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
 
-MYSQL_HOST=mysql
-
-# create openmrs properties file
-echo "Creating openmrs properties file"
-
-if [ ! -f /opt/tomcat/.OpenMRS/openmrs-runtime.properties ]; then
-	cat > /opt/tomcat/.OpenMRS/openmrs-runtime.properties <<- EOF
-		connection.username=${MYSQL_OPENMRS_USER}
-		connection.password=${MYSQL_OPENMRS_PASSWORD}
-		connection.url=jdbc:mysql://${MYSQL_HOST}:3306/${MYSQL_OPENMRS_DATABASE}?autoReconnect=true&sessionVariables=storage_engine=InnoDB&useUnicode=true&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull
-		module.allow_web_admin=true
-		auto_update_database=false
-		sync.mandatory=false
-	EOF
-
-	echo "Finished creating openmrs properties file"
-
-fi
-
 while [ ! -f /etc/migrations/.postgres_migrations_complete -o ! -f /etc/migrations/.mysql_migrations_complete ]; 
  do echo 'waiting for database migrations to complete...'; 
  sleep 5; 
